0aae48ca6082cbce1db5ff9728e3bd5dba6449ac
Bernhard Göschlberger <bernhard.goeschlberger@researchstudio.at>
2021-11-18 09:54:04 +0100

    changed greeting test

--- test_greeting_logic.py
+++ test_greeting_logic.py
@@ -4,4 +4,4 @@ from greeting_logic import GreetingGenerator
 
 def test_get_greeting():
     gg = GreetingGenerator()
-    assert gg.get_greeting() == "hello world!"
+    assert gg.get_greeting() == "good morning world!"
